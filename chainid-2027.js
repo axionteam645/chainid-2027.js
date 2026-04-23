77c7c8f70b55ec4e0f3faff4c9dd3d48f3b8a8d9
AXION TEAM <axionteam@axiondefi.com>
2026-04-23 16:14:09 +0300

    Create chainid-2027.js

---
+++ chainid-2027.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "AXION Mainnet",
+  chain: "AXION",
+  rpc: [
+    "https://rpc.axiondefi.com"
+  ],
+  explorers: [
+    {
+      name: "axscan",
+      url: "https://axscan.axiondefi.com",
+      standard: "EIP3091"
+    }
+  ]
+}
